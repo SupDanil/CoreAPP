e33f0c91b61ea7271d672672f2f314f7ef5edf93
com.Kondratov <com.Kondratov@gmail.com>
2020-10-03 22:41:45 +0300

    Добавил подключение к базе данных,доработал визуальную чать,создал модель.

--- CoreApp/Controllers/HomeController.cs
+++ CoreApp/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CoreApp.Models;
+using CoreApp.Data;
 
 namespace CoreApp.Controllers
 {
     public class HomeController : Controller
     {
+         //var rnd = new Random(DateTime.Now.Millisecond);
+        private readonly NickNameContext db;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NickNameContext context)
         {
             _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
@@ -23,6 +28,13 @@ namespace CoreApp.Controllers
             return View();
         }
 
-        
+        [HttpPost]
+        public IActionResult Index(string Name)
+        {
+            
+            return View();
+        }
+
+
     }
 }
---
+++ CoreApp/Data/NickNameContext.cs
@@ -0,0 +1,19 @@
+﻿using CoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Data
+{
+    public class NickNameContext : DbContext
+    {
+            public DbSet<NickName> NickNames { get; set; }
+            public NickNameContext(DbContextOptions<NickNameContext> options)
+                : base(options)
+            {
+                Database.EnsureCreated();   
+            }
+    }
+}
